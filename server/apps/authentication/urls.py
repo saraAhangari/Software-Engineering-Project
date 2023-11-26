61afadd55249fe84d86e08623da93b240c571738
Saeed Zare <saeed1381@gmail.com>
2023-11-26 23:25:29 +0330

    config jwt token in settings

--- server/apps/authentication/urls.py
+++ server/apps/authentication/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView
+from .views import RegisterView, LogoutView, OtpGenerator, GetToken
 
 
 urlpatterns = [
-    path('register', RegisterView.as_view()),
-    path('login', LoginView.as_view()),
+    path('otp/generate', OtpGenerator.as_view()),
+    path('get_token', GetToken.as_view()),
     path('logout', LogoutView.as_view())
 ]
