7c729467266d891fe9748f45b3ec04ee3d11320a
Saeed Zare <saeed1381@gmail.com>
2023-11-26 11:50:29 +0330

    add index url

--- server/hospitalAppointment/urls.py
+++ server/hospitalAppointment/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import Index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('apps.authentication.urls'))
+    path('api/v1/', include('apps.authentication.urls')),
+    path('', Index.as_view())
 ]
