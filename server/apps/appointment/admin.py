343cac8823c80c383e7ade19c15c8b516bf221a7
ZedEsm <zedesm23@gmail.com>
2023-12-06 14:07:55 -0800

    add admin of timeslice, doctor, speciality

--- server/apps/appointment/admin.py
+++ server/apps/appointment/admin.py
@@ -1,22 +1,43 @@
 from django.contrib import admin
-from .models import Doctor, Speciality
+from import_export.admin import ImportExportModelAdmin
+
+from .models import Doctor, Speciality, TimeSlice
 from ..authentication.models import Role
 
 
+@admin.register(Doctor)
 class DoctorAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'username', 'email', 'description', 'fees', 'medical_system_number', 'display_specialities')
-    list_filter = ('speciality',)
+        'id', 'username', 'email', 'description', 'fees', 'slice', 'medical_system_number',
+        'display_specialities',)
+    list_filter = ('speciality', 'first_name', 'last_name',)
 
     def display_specialities(self, obj):
         return ", ".join([speciality.name for speciality in obj.speciality.all()])
 
     display_specialities.short_description = 'Specialities'
 
+    search_fields = ('speciality',)
 
+    class Meta:
+        model = Doctor
+
+
+@admin.register(Speciality)
 class SpecialityAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
+    list_filter = ('id', 'name')
+    search_fields = ('id', 'name')
+
+    class Meta:
+        model = Speciality
 
 
-admin.site.register(Speciality, SpecialityAdmin)
-admin.site.register(Doctor, DoctorAdmin)
+@admin.register(TimeSlice)
+class TimeSliceAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'date', 'start', 'end', 'status')
+    list_filter = ('date', 'start', 'end', 'status')
+    search_fields = ('date', 'start', 'end', 'status')
+
+    class Meta:
+        model = TimeSlice
