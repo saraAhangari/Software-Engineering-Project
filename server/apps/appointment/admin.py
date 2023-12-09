5e3c36894e43c38d0b05b72f545de67934f7020c
ZedEsm <zedesm23@gmail.com>
2023-12-09 03:26:36 -0800

    add admin of assurance, patient, patient medical history, medicine , appointment

--- server/apps/appointment/admin.py
+++ server/apps/appointment/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 
-from .models import Doctor, Speciality, TimeSlice
-from ..authentication.models import Role
+from .models import Doctor, Speciality, TimeSlice, Assurance, PatientMedicalHistory, Patient, Medicine, Appointment
 
 
 @admin.register(Doctor)
@@ -10,7 +9,7 @@ class DoctorAdmin(admin.ModelAdmin):
     list_display = (
         'id', 'username', 'email', 'description', 'fees', 'slice', 'medical_system_number',
         'display_specialities',)
-    list_filter = ('speciality', )
+    list_filter = ('speciality',)
 
     def display_specialities(self, obj):
         return ", ".join([speciality.name for speciality in obj.speciality.all()])
@@ -19,6 +18,7 @@ class DoctorAdmin(admin.ModelAdmin):
 
     search_fields = ('speciality', 'first_name', 'last_name')
     exclude = ('password', 'groups', 'last_login', 'is_active', 'date_joined', 'is_staff')
+    list_per_page = 25
 
     class Meta:
         model = Doctor
@@ -41,3 +41,56 @@ class TimeSliceAdmin(ImportExportModelAdmin):
 
     class Meta:
         model = TimeSlice
+
+
+@admin.register(Assurance)
+class AssuranceAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'name')
+    list_filter = ('name',)
+
+    class Meta:
+        model = Assurance
+
+
+@admin.register(PatientMedicalHistory)
+class PatientMedicalHistoryAdmin(ImportExportModelAdmin):
+    fields = ('height', 'weight', 'blood_group', 'blood_pressure')
+    list_display = ('id', 'height', 'weight', 'blood_group', 'blood_pressure')
+    search_fields = ('blood_group',)
+    list_per_page = 25
+
+    class Meta:
+        model = PatientMedicalHistory
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'first_name', 'last_name', 'assurance', 'medical_history')
+    exclude = ('password', 'groups', 'last_login', 'is_active', 'date_joined', 'is_staff')
+    list_per_page = 25
+
+    class Meta:
+        model = Patient
+
+
+@admin.register(Medicine)
+class MedicineAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'generic_name', 'infant_safe', 'price',)
+    search_fields = ('generic_name',)
+
+    class Meta:
+        model = Medicine
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'patient_id', 'doctor_id', 'date', 'description', 'status', 'type')
+    search_fields = ('type', 'status')
+    list_per_page = 25
+    list_filter = ('type', 'status')
+
+    class Meta:
+        model = Appointment
+
+
+
