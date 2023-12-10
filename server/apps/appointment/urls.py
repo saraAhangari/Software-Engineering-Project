4dc10258c253ccdb3be959ebd108dfcef65ea786
ZedEsm <zedesm23@gmail.com>
2023-12-10 05:46:47 -0800

    add path of saving comment

--- server/apps/appointment/urls.py
+++ server/apps/appointment/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import AssuranceView, DoctorView
-
+from .views import AssuranceView, DoctorView, CommentView
 
 urlpatterns = [
     path('assurance', AssuranceView.as_view()),
     path('doctor', DoctorView.as_view()),
+    path('comment/<int:doctor_id>/', CommentView.as_view(), name='save_comment'),
 ]
