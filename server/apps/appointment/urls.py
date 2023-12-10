892b71fbed9db04b96aa10cb2894779d9f6f7f8e
ZedEsm <zedesm23@gmail.com>
2023-12-10 13:09:46 -0800

    make some refactoring

--- server/apps/appointment/urls.py
+++ server/apps/appointment/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from .views import AssuranceView, DoctorView, CommentView
+from .views import AssuranceView,  CommentView, PatientView, DoctorDetailView, DoctorListView
 
 urlpatterns = [
+    # assurance
     path('assurance', AssuranceView.as_view()),
-    path('doctors/<int:doctor_id>/', DoctorView.as_view(), name='doctor-detail'),
-    path('doctors', DoctorView.as_view(), name='doctor-list'),
+
+    # doctor
+    path('doctors/<int:doctor_id>/', DoctorDetailView.as_view(), name='doctor-detail'),
+    path('doctors', DoctorListView.as_view(), name='doctor-list'),
+
+    # comment
     path('comment/<int:doctor_id>/', CommentView.as_view(), name='save_comment'),
+    path('comments', CommentView.as_view(), name='get_comment'),
+
+    # patient
+    path('patient/profile', PatientView.as_view())
 ]
