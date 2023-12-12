afa8ef17bd5b9d2db02f5d33b8647e54c05252ff
Saeed Zare <saeed1381@gmail.com>
2023-12-13 01:43:59 +0330

    add swagger routes

--- server/hospitalAppointment/urls.py
+++ server/hospitalAppointment/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import Index
 from schema_graph.views import Schema
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path("schema/", Schema.as_view()),
@@ -9,4 +10,8 @@ urlpatterns = [
     path('api/v1/', include('apps.authentication.urls')),
     path('api/v1/', include('apps.appointment.urls')),
     path('', Index.as_view()),
+    # Swagger
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
