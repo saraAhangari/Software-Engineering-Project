d7665e718ee0796c3fa28d0e87b0b7fc7912161b
mm.mazidabadi <m3farahani1382@gmail.com>
2023-12-14 20:16:06 +0330

    fix(front-end): Fixes main template component.

--- front-end/src/components/container/MainTemplate.js
+++ front-end/src/components/container/MainTemplate.js
@@ -38,69 +38,69 @@ function MainTemplate(
     }
 
     return (
-        <div className={'template'}
-             style={
-                 {
-                     backgroundImage: `url(${showDefaultBackground && DEFAULT_BACKGROUND})`
+        <>
+            <div className={'template'}
+                 style={
+                     {
+                         backgroundImage: `url(${showDefaultBackground && DEFAULT_BACKGROUND})`
+                     }
                  }
-             }
-        >
-            <div className={'template__gray-filter'}>
-                <div
-                    className={'container'}
-                    style={
-                        {
-                            paddingTop: height,
-                        }
-                    }
-                >
-                    <AppBar ref={ref} className={'appbar'}>
-                        <Toolbar className={'toolbar'}>
-                            <IconButton onClick={navigateToHome}>
-                                <img className={'toolbar__icon'}
-                                     src={ICON_HOME}
-                                />
-                            </IconButton>
-                            <div className={'toolbar__icon-spacer'}/>
-                            {
-                                hasLoggedIn ? (
-                                    <>
-                                        <IconButton onClick={navigateToProfile}>
-                                            <img className={'toolbar__icon'}
-                                                 src={ICON_PROFILE}
-                                            />
-                                        </IconButton>
-                                        <div
-                                            className={'toolbar__icon-divider'}
-                                            style={{backgroundColor: theme.palette.divider}}
-                                        />
-                                        <IconButton onClick={logout}>
-                                            <img className={'toolbar__icon'}
-                                                 src={ICON_LOGOUT}
-                                            />
-                                        </IconButton>
-                                    </>
-                                ) : (
-                                    buttonTitle && <Button className={'toolbar__button'}
-                                                           onClick={onButtonClicked}>{buttonTitle}</Button>
-                                )
-                            }
-                        </Toolbar>
-                    </AppBar>
+            />
+            <div className={'gray-filter'}/>
+            <div
+                className={'container'}
+                style={
                     {
-                        children && cloneElement(
-                            children,
-                            {
-                                style: {
-                                    ...children.props.style,
-                                    flexGrow: 1
-                                }
-                            }
-                        )
+                        paddingTop: height,
                     }
-                </div>
+                }
+            >
+                <AppBar ref={ref} className={'appbar'}>
+                    <Toolbar className={'toolbar'}>
+                        <IconButton onClick={navigateToHome}>
+                            <img className={'toolbar__icon'}
+                                 src={ICON_HOME}
+                            />
+                        </IconButton>
+                        <div className={'toolbar__icon-spacer'}/>
+                        {
+                            hasLoggedIn ? (
+                                <>
+                                    <IconButton onClick={navigateToProfile}>
+                                        <img className={'toolbar__icon'}
+                                             src={ICON_PROFILE}
+                                        />
+                                    </IconButton>
+                                    <div
+                                        className={'toolbar__icon-divider'}
+                                        style={{backgroundColor: theme.palette.divider}}
+                                    />
+                                    <IconButton onClick={logout}>
+                                        <img className={'toolbar__icon'}
+                                             src={ICON_LOGOUT}
+                                        />
+                                    </IconButton>
+                                </>
+                            ) : (
+                                buttonTitle && <Button className={'toolbar__button'}
+                                                       onClick={onButtonClicked}>{buttonTitle}</Button>
+                            )
+                        }
+                    </Toolbar>
+                </AppBar>
+                {
+                    children && cloneElement(
+                        children,
+                        {
+                            style: {
+                                ...children.props.style,
+                                flexGrow: 1
+                            }
+                        }
+                    )
+                }
             </div>
-        </div>
+        </>
     )
 }
 
