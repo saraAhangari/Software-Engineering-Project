7b97a1761e454d26358adca9407ad3bcce360ded
Saeed Zare <saeed1381@gmail.com>
2024-01-28 10:29:33 +0330

    automated test adds

--- server/apps/appointment/tests/test_views.py
+++ server/apps/appointment/tests/test_views.py
@@ -44,27 +44,47 @@ from apps.authentication.models import Role
 #
 #         self.assertEqual(response.status_code, 200)
 
-#
-# class DoctorListView(TestCase):
-#     @classmethod
-#     def setUpTestData(cls):
-#         call_command(command_name='create_roles')
-#         Doctor.objects.create(first_name='محمد', last_name='محمدی', national_id='4314314312',
-#                               phone_no='4314314312', birthdate='2002-12-23', gender='m',
-#                               role=Role.objects.get(name='doctor'), slice=40, description='',
-#                               medical_system_number=585989).save()
-#
-#         Doctor.objects.create(first_name='مهدی', last_name='عقیلی', national_id='1235621324',
-#                               phone_no='1235621324', birthdate='2002-12-23', gender='m',
-#                               role=Role.objects.get(name='doctor'), slice=40, description='',
-#                               medical_system_number=978457).save()
-#
-#         Doctor.objects.create(first_name='علی', last_name='علوی', national_id='4690012345',
-#                               phone_no='4690012345', birthdate='2002-12-23', gender='m',
-#                               role=Role.objects.get(name='doctor'), slice=40, description='',
-#                               medical_system_number=754874).save()
-#
-#     def test_set_doctor_time_slices(self):
-#         response = self.client.get('/api/v1/doctors')
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(len(response.json()), 3)
+
+class DoctorListView(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        call_command(command_name='create_roles')
+        Doctor.objects.create(first_name='محمد', last_name='محمدی', national_id='4314314312', username='4314314312',
+                              phone_no='4314314312', birthdate='2002-12-23', gender='m',
+                              role=Role.objects.get(name='doctor'), slice=40, description='',
+                              medical_system_number=585989).save()
+
+        Doctor.objects.create(first_name='مهدی', last_name='عقیلی', national_id='1235621324', username='1235621324',
+                              phone_no='1235621324', birthdate='2002-12-23', gender='m',
+                              role=Role.objects.get(name='doctor'), slice=40, description='',
+                              medical_system_number=978457).save()
+
+        Doctor.objects.create(first_name='علی', last_name='علوی', national_id='4690012345', username='4690012345',
+                              phone_no='4690012345', birthdate='2002-12-23', gender='m',
+                              role=Role.objects.get(name='doctor'), slice=40, description='',
+                              medical_system_number=754874).save()
+
+    def test_get_doctor_list(self):
+        response = self.client.get('/api/v1/doctors')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 3)
+
+    def test_get_doctor_list_by_first_name(self):
+        response = self.client.get('/api/v1/doctors?first_name=م')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 2)
+
+    def test_get_doctor_list_by_last_name(self):
+        response = self.client.get('/api/v1/doctors?first_name=ع')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 1)
+
+    def test_get_doctor_limit_offset(self):
+        response = self.client.get('/api/v1/doctors?limit=2&offset=1')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 2)
+
+    def test_get_doctor_offset(self):
+        response = self.client.get('/api/v1/doctors?limit=2&offset=0')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 2)
