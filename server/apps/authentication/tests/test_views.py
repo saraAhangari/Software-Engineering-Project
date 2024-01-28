095ea6404400783e445d16fed88701d39c5e740b
Saeed Zare <saeed1381@gmail.com>
2024-01-28 15:03:29 +0330

    tests updated

--- server/apps/authentication/tests/test_views.py
+++ server/apps/authentication/tests/test_views.py
@@ -65,84 +65,84 @@ class AuthenticationViewTest(TestCase):
         response = self.client.post(reverse('validate'))
         self.assertEqual(response.status_code, 400)
 
-    # @override_settings(CACHES={'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}})
-    # def test_success_patient_validate(self):
-    #     person = {
-    #         "first_name": "ali",
-    #         "last_name": "alavi",
-    #         "national_id": "4480158693",
-    #         "phone_no": "09140887539",
-    #         "birthdate": "2023-12-21",
-    #         "gender": "m",
-    #     }
-    #
-    #     response = self.client.post('/api/v1/validate', data=person)
-    #
-    #     self.assertEqual(response.json(), {'message': 'کد یکبار مصرف ارسال شد.'})
-    #     self.assertEqual(response.status_code, 200)
-    #
-    # @override_settings(CACHES={'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}})
-    # def test_duplicate_patient_register(self):
-    #     Patient.objects.get_or_create(first_name='akbar', last_name='akbari', national_id='4480158693',
-    #                                   phone_no='1234567899', birthdate='2002-12-23', gender='m',
-    #                                   role=Role.objects.get(name='patient'))
-    #
-    #     person = {
-    #         "first_name": "naghi",
-    #         "last_name": "naghavi",
-    #         "national_id": "4480158693",
-    #         "phone_no": "12343211234",
-    #         "birthdate": "2023-12-10",
-    #         "gender": "f",
-    #     }
-    #
-    #     response = self.client.post('/api/v1/validate', data=person)
-    #     self.assertEqual(response.status_code, 400)
-    #
-    # @override_settings(CACHES={'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}})
-    # def test_success_patient_register_process(self):
-    #     person = {
-    #         "first_name": "saeed",
-    #         "last_name": "zare",
-    #         "national_id": "4480158693",
-    #         "phone_no": "09140887539",
-    #         "birthdate": "2023-12-21",
-    #         "gender": "m",
-    #     }
-    #
-    #     response = self.client.post('/api/v1/validate', data=person)
-    #
-    #     self.assertEqual(response.json(), {'message': 'کد یکبار مصرف ارسال شد.'})
-    #     self.assertEqual(response.status_code, 200)
-    #
-    #     person['otp'] = cache.get(person['phone_no'])
-    #     response = self.client.post('/api/v1/register', data=person)
-    #
-    #     self.assertEqual(response.status_code, 201)
-    #     self.assertIsNotNone(response.json())
-    #     self.assertIsNotNone(response.json()['id'])
-    #     self.assertEqual(response.json()['first_name'], person['first_name'])
-    #     self.assertEqual(response.json()['last_name'], person['last_name'])
-    #     self.assertEqual(response.json()['national_id'], person['national_id'])
-    #     self.assertEqual(response.json()['birthdate'], person['birthdate'])
-    #     self.assertEqual(response.json()['gender'], person['gender'])
-    #
-    # @override_settings(CACHES={'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}})
-    # def test_success_patient_login_process(self):
-    #     Patient.objects.get_or_create(first_name='akbar', last_name='akbari', national_id='1234567899',
-    #                                   phone_no='1234567899', birthdate='2002-12-23', gender='m',
-    #                                   role=Role.objects.get(name='patient'))
-    #
-    #     response = self.client.post('/api/v1/login', data={'national_id': '1234567899'})
-    #
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(response.json(), {'message': 'کد یکبار مصرف ارسال شد.'})
-    #
-    #     response = self.client.post('/api/v1/get_token',
-    #                                 data={'national_id': '1234567899', 'otp': cache.get('1234567899')})
-    #
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertIsNotNone(response.json()['access_token'])
+    @override_settings(CACHES={'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}})
+    def test_success_patient_validate(self):
+        person = {
+            "first_name": "ali",
+            "last_name": "alavi",
+            "national_id": "4480158693",
+            "phone_no": "09140887539",
+            "birthdate": "2023-12-21",
+            "gender": "m",
+        }
+
+        response = self.client.post('/api/v1/validate', data=person)
+
+        self.assertEqual(response.json(), {'message': 'کد یکبار مصرف ارسال شد.'})
+        self.assertEqual(response.status_code, 200)
+
+    @override_settings(CACHES={'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}})
+    def test_duplicate_patient_register(self):
+        Patient.objects.get_or_create(first_name='akbar', last_name='akbari', national_id='4480158693',
+                                      phone_no='1234567899', birthdate='2002-12-23', gender='m',
+                                      role=Role.objects.get(name='patient'))
+
+        person = {
+            "first_name": "naghi",
+            "last_name": "naghavi",
+            "national_id": "4480158693",
+            "phone_no": "12343211234",
+            "birthdate": "2023-12-10",
+            "gender": "f",
+        }
+
+        response = self.client.post('/api/v1/validate', data=person)
+        self.assertEqual(response.status_code, 400)
+
+    @override_settings(CACHES={'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}})
+    def test_success_patient_register_process(self):
+        person = {
+            "first_name": "saeed",
+            "last_name": "zare",
+            "national_id": "4480158693",
+            "phone_no": "09140887539",
+            "birthdate": "2023-12-21",
+            "gender": "m",
+        }
+
+        response = self.client.post('/api/v1/validate', data=person)
+
+        self.assertEqual(response.json(), {'message': 'کد یکبار مصرف ارسال شد.'})
+        self.assertEqual(response.status_code, 200)
+
+        person['otp'] = cache.get(person['phone_no'])
+        response = self.client.post('/api/v1/register', data=person)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertIsNotNone(response.json())
+        self.assertIsNotNone(response.json()['id'])
+        self.assertEqual(response.json()['first_name'], person['first_name'])
+        self.assertEqual(response.json()['last_name'], person['last_name'])
+        self.assertEqual(response.json()['national_id'], person['national_id'])
+        self.assertEqual(response.json()['birthdate'], person['birthdate'])
+        self.assertEqual(response.json()['gender'], person['gender'])
+
+    @override_settings(CACHES={'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}})
+    def test_success_patient_login_process(self):
+        Patient.objects.get_or_create(first_name='akbar', last_name='akbari', national_id='1234567899',
+                                      phone_no='1234567899', birthdate='2002-12-23', gender='m',
+                                      role=Role.objects.get(name='patient'))
+
+        response = self.client.post('/api/v1/login', data={'national_id': '1234567899'})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'message': 'کد یکبار مصرف ارسال شد.'})
+
+        response = self.client.post('/api/v1/get_token',
+                                    data={'national_id': '1234567899', 'otp': cache.get('1234567899')})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.json()['access_token'])
 
     def test_success_logout(self):
         pass
